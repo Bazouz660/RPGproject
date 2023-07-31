3c0c8828389a1f5705202d2ddf3177661167a060
Bazouz660 <basile.trebus-hamann@epitech.eu>
2023-07-31 16:38:11 +0200

    [FIX]: -> restored main because it vanished for some reason

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** RPG3
+** File description:
+** main
+*/
+
+#include "Engine.hpp"
+#include "logger.hpp"
+
+int main(int argc, char **argv)
+{
+    bya::Engine& engine = bya::Engine::getInstance();
+
+    try {
+        engine.preProcessor();
+        engine.run();
+    } catch (std::exception &e) {
+        bya::logger::error(e.what());
+        return 84;
+    }
+    return 0;
+}
